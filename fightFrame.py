d2158bbdc153c2e918082e48c23cfb2112dcb08a
sophisticate9008 <94435821+sophisticate9008@users.noreply.github.com>
2022-08-01 13:21:16 +0800

    Update fightFrame.py

--- fightFrame.py
+++ fightFrame.py
@@ -232,10 +232,16 @@ def   fight(  left,  right,  isDispaly,  two) :
                     skill_a( two, i, isDispaly, txts, count)
                     
                     if skill_p( two, i, 1, isDispaly, 0, txts, count):
+                        if isDispaly == 1:
+                            image_add_text(count, txts,  text_color=(0, 0, 0), text_size=12)
+                        txts.clear()    
                         return two
                     #挑选对应进攻被动
 
                     if calLife( two, i, isDispaly, ordinal_0, ordinal_1, 1, 0, txts, count):
+                        if isDispaly == 1:
+                            image_add_text(count, txts,  text_color=(0, 0, 0), text_size=12)
+                        txts.clear()
                         return two
                     calState( two, isDispaly, txts)#计算状态
                     if two[ordinal_1].life <= 0 :
@@ -272,9 +278,15 @@ def   fight(  left,  right,  isDispaly,  two) :
                             txts.append("{}进行普攻".format (two[ordinal_0].name))
                                                                     
                     if skill_p( two, i, 1, isDispaly, 0, txts, count) :
+                        if isDispaly == 1:
+                            image_add_text(count, txts,  text_color=(0, 0, 0), text_size=12)
+                        txts.clear()
                         return two
                     #挑选对应进攻被动
                     if calLife( two, i, isDispaly, ordinal_0, ordinal_1, 1, two[ordinal_0].mess, txts, count):
+                        if isDispaly == 1:
+                            image_add_text(count, txts,  text_color=(0, 0, 0), text_size=12)
+                        txts.clear()
                         return two
                     calState( two, isDispaly, txts) 
                     if two[ordinal_1].life <= 0:
