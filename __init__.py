6253d56cec6d5f2e64983445a643897db8733841
sophisticate9008 <94435821+sophisticate9008@users.noreply.github.com>
2022-08-03 22:50:46 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -89,26 +89,12 @@ async def _(bot: Bot,
     if list_prob[3] < 100:
         list_beilv.append(100.00)
     else:
-        list_beilv.append(10000 / list_prob[3]) 
-    try:
-        msg_id = await bot.send(event, '随机到的两名英桀是\n{}  {}\n胜率分别为{:.2f}  {:.2f}\n 获胜获得金币倍率分别为{:.2f}  {:.2f}'.format(list_prob[0], list_prob[2], float(list_prob[1] /10000), float(list_prob[3] / 10000), float(list_beilv[0]), float(list_beilv[1])))
-        withdraw_message_manager.withdraw_message(
-            event,
-            msg_id["message_id"],
-            Config.get_config("fight", FIGHT_TMP),
-        )  
-    except:
-        pass
+        list_beilv.append(10000 / list_prob[3])
+    global msg_id_0 
+    msg_id_0 = await bot.send(event, '随机到的两名英桀是\n{}  {}\n胜率分别为{:.2f}  {:.2f}\n 获胜获得金币倍率分别为{:.2f}  {:.2f}'.format(list_prob[0], list_prob[2], float(list_prob[1] /10000), float(list_prob[3] / 10000), float(list_beilv[0]), float(list_beilv[1])))
 
-    try:
-        msg_id = await bot.send(event, '请选择你的支持目标和投注金额, 0为前 1为后, 两个参数空格隔开')
-        withdraw_message_manager.withdraw_message(
-            event,
-            msg_id["message_id"],
-            Config.get_config("fight", FIGHT_TMP),
-        )  
-    except:
-        pass
+    global msg_id_1
+    msg_id_1 = await bot.send(event, '请选择你的支持目标和投注金额, 0为前 1为后, 两个参数空格隔开')
     state['role_two'] = [rands1, rands2]
     state['beilv'] = list_beilv
     @ready.got('select')
@@ -165,6 +151,20 @@ async def _(bot: Bot,
                 event,
                 msg_id["message_id"],
                 Config.get_config("fight", "FIGHT_PROCESS"),
+            )
+        except:
+            pass
+
+        try:
+            withdraw_message_manager.withdraw_message(
+                event,
+                msg_id_0["message_id"],
+                Config.get_config("fight", "FIGHT_TMP),
+            )
+            withdraw_message_manager.withdraw_message(
+                event,
+                msg_id_1["message_id"],
+                Config.get_config("fight", "FIGHT_TMP"),
             )
         except:
             pass
