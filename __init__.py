65ac656376a3de851fb4685a6cd0d647502444bd
sophisticate9008 <94435821+sophisticate9008@users.noreply.github.com>
2022-09-27 16:28:43 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -224,9 +224,9 @@ async def _(bot: Bot,
             fight_single[group][uid] = {}
             await ready.finish("参数不正确,消耗掉一次机会,若开始请重新输入【海滨乱斗】")
         gold_have = await BagUser.get_gold(uid, group)
-        if gold_have < money_spend:
+        if gold_have < money_spend and money_spend < 0:
             fight_single[group][uid] = {}
-            await ready.finish("你的钱不够,请下次看好你有多少金币，若开始请重新输入【海滨乱斗】")
+            await ready.finish("你的钱不够或为负数,请下次看好你有多少金币并输入正数")
         await BagUser.spend_gold(uid, group, money_spend)
         list_fight = []
         list_role = fight_single[group][uid]['role_two']
